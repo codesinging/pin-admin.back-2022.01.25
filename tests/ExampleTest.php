498f543ca0e8069d7fff5c75355d15ca25965eaf
codesinging <codesinging@gmail.com>
2021-10-05 08:06:32 +0800

    Add: test example

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CodeSinging\PinAdmin\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_example()
+    {
+        self::assertTrue(true);
+    }
+}
